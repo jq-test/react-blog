b01b5a2e0abf05b4847ed80147683bb24faba17a
JQ <jqjqshops@gmail.com>
2024-11-23 16:45:34 -0500

    Setting up React Project 7 - theme context, providers, and settings

---
+++ my-blog/src/providers/AppProviders.jsx
@@ -0,0 +1,21 @@
+//Share state and logics with all child components, so accessible w/o passing props.
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { PreferencesProvider } from '../contexts/PreferencesContext';
+import { BlogProvider } from '../contexts/BlogContext';
+import PropTypes from 'prop-types';
+
+export function AppProviders({ children }) {
+  return (
+    <ThemeProvider>
+      <PreferencesProvider>
+        <BlogProvider>
+          {children}
+        </BlogProvider>
+      </PreferencesProvider>
+    </ThemeProvider>
+  );
+}
+
+AppProviders.propTypes = {
+  children: PropTypes.node.isRequired
+};
